72bc00d1786c1aca4358dcc9f568aa2199fdfbeb
El-Do-RaDo <manjaniket@gmail.com>
2020-12-26 15:46:28 +0530

    adding deque files to git.

--- dequeue/palindrome.py
+++ dequeue/palindrome.py
@@ -5,10 +5,10 @@ def palidrome_checker(a_string):
 
     for ch in a_string:
         char_deque.add_rear(ch)
-//    still_equal = True
-//
-//    while char_deque.size() > 1 and still_equal:
-//        first = char_deque.remove_front()
+    still_equal = True
+
+    while char_deque.size() > 1 and still_equal:
+        first = char_deque.remove_front()
 //        last = char_deque.remove_rear()
 //        if first != last:
 //            still_equal = False
